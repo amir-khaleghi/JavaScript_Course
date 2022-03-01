2a6265a0376012c4c8c197394321131562a07180
amir-khaleghi <a.khaleghi.d@gmail.com>
2022-03-01 23:41:54 +0330

    Basic Operators

--- Fudamentals_part01/script.js
+++ Fudamentals_part01/script.js
@@ -1,13 +1,29 @@
 /* -------------------------------------------------------------------------- */
-/*                             let, const and var                             */
+/*                               Basic Operators                              */
 /* -------------------------------------------------------------------------- */
-/* ----------------------------------- let ---------------------------------- */
-let age = 30;
-age = 31;
-/* ---------------------------------- const --------------------------------- */
-const birthday = 1991;
-// const is immutable
 
-/* ----------------------------------- var ---------------------------------- */
-var job = "programmer"
-job = "teacher"
+/* ----------------------------- math operators ----------------------------- */
+const now = 2050;
+const ageAmir = now - 1994;
+const ageEli = now - 1988;
+console.log(ageAmir, ageEli);
+
+console.log(ageAmir * 2, ageEli / 10, 2 ** 3);
+
+const firstName = "Amir"
+const lastName = "Khaleghi"
+console.log(firstName + " " + lastName);
+
+/* -------------------------- Assignment operators -------------------------- */
+let x = 10 + 5;
+x += 10;
+x *= 4;
+x++;
+x--;
+x--;
+console.log(x);
+
+/* -------------------------- comparison operators -------------------------- */
+// >, < , >= , <=
+console.log(ageAmir > ageEli);
+console.log(ageAmir <= 29);
