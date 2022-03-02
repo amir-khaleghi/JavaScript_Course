d0f0a56afa385cb1ce58010ce25dd15e6e5b4b2e
amir-khaleghi <a.khaleghi.d@gmail.com>
2022-03-02 16:49:57 +0330

    Truthy and Falsy Values

--- Fudamentals_part01/script.js
+++ Fudamentals_part01/script.js
@@ -1,21 +1,22 @@
 /* -------------------------------------------------------------------------- */
-/*                        Type Conversion and Coercion•                       */
+/*                           Truthy and Falsy Values                          */
 /* -------------------------------------------------------------------------- */
-/* ----------------------------- type conversion ---------------------------- */
-const inputYear = '1991';
-console.log(Number(inputYear), inputYear);
-console.log(Number(inputYear) + 18);
-console.log(Number('Jonas'));
-console.log(typeof NaN);
-console.log(String(23), 23);
-/* ------------------------------ type coercion ----------------------------- */
-console.log('I am ' + 23 + ' years old');
-console.log('23' - '10' - 3);
-console.log('23' / '2');
 
-let n = '1' + 1; // '11' 
-n = n - 1;
-console.log(n);
-console.log("2" - 1 + 5)
-console.log(1 - "2" + 5)
-console.log("2" + "2" - 1 + 5 + "2")
+/*NOTE: --------------- 5 falsy values: 0, '', undefined, null, NaN -------------- */
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Jonas'));
+console.log(Boolean({}));
+console.log(Boolean(''));
+const money = 100;
+if (money) {
+    console.log("Don't spend it all ;)");
+} else {
+    console.log('You should get a job!');
+}
+let height = 0;
+if (height) {
+    console.log('YAY! Height is defined');
+} else {
+    console.log('Height is UNDEFINED');
+}
