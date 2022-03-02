75add1fdcc9e18e5b21cfe93dc94f3560190ece9
amir-khaleghi <a.khaleghi.d@gmail.com>
2022-03-02 15:36:12 +0330

    Coding Challenge #01 (BMI)

--- Fudamentals_part01/script.js
+++ Fudamentals_part01/script.js
@@ -1,29 +1,35 @@
 /* -------------------------------------------------------------------------- */
-/*                               Basic Operators                              */
+/*                            Coding Challenge #01 (BMI)                            */
 /* -------------------------------------------------------------------------- */
 
-/* ----------------------------- math operators ----------------------------- */
-const now = 2050;
-const ageAmir = now - 1994;
-const ageEli = now - 1988;
-console.log(ageAmir, ageEli);
+/*
+Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / height ** 2 = mass / (height * height). (mass in kg and height in meter).
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs using the formula (you can even implement both versions)
+3. Create a boolean variable 'markHigherBMI' containing information about whether Mark has a higher BMI than John.
+TEST DATA 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
+TEST DATA 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 m tall.
+GOOD LUCK 😀
+*/
+/* ------------------------------- test data 1 ------------------------------ */
+// const markWeight = 78;
+// const markHeight = 1.69;
+// const johnWeight = 92;
+// const johnHeight = 1.95;
 
-console.log(ageAmir * 2, ageEli / 10, 2 ** 3);
+/* ------------------------------- test data 2 ------------------------------ */
+const markWeight = 95;
+const markHeight = 1.88;
+const johnWeight = 85;
+const johnHeight = 1.76;
 
-const firstName = "Amir"
-const lastName = "Khaleghi"
-console.log(firstName + " " + lastName);
+/* --------------------------------- bmi formula -------------------------------- */
+const markBMI = markWeight / markHeight ** 2
+const johnBMI = johnWeight / johnHeight ** 2
 
-/* -------------------------- Assignment operators -------------------------- */
-let x = 10 + 5;
-x += 10;
-x *= 4;
-x++;
-x--;
-x--;
-console.log(x);
-
-/* -------------------------- comparison operators -------------------------- */
-// >, < , >= , <=
-console.log(ageAmir > ageEli);
-console.log(ageAmir <= 29);
+/* ------------------------------ comparing bmi ----------------------------- */
+if (markBMI > johnBMI) markHigherBMI1 = true
+else markHigherBMI1 = false
+/* ----------------------------------- or BETTER WAY ----------------------------------- */
+const markHigherBMI = markBMI > johnBMI
+console.log(markBMI, johnBMI, markHigherBMI)
