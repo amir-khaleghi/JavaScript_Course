21e1b4c798bc10e9183a2d682e94ca8afc41de66
amir-khaleghi <a.khaleghi.d@gmail.com>
2022-03-02 15:54:09 +0330

    Strings and Template Literals

--- Fudamentals_part01/script.js
+++ Fudamentals_part01/script.js
@@ -1,35 +1,25 @@
 /* -------------------------------------------------------------------------- */
-/*                            Coding Challenge #01 (BMI)                            */
+/*                        Strings and Template Literals                       */
 /* -------------------------------------------------------------------------- */
+const firstName = 'Jonas';
+const job = 'teacher';
+const birthYear = 1991;
+const year = 2037;
+/* ----------------------------- create template ---------------------------- */
 
-/*
-Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / height ** 2 = mass / (height * height). (mass in kg and height in meter).
-1. Store Mark's and John's mass and height in variables
-2. Calculate both their BMIs using the formula (you can even implement both versions)
-3. Create a boolean variable 'markHigherBMI' containing information about whether Mark has a higher BMI than John.
-TEST DATA 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
-TEST DATA 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 m tall.
-GOOD LUCK 😀
-*/
-/* ------------------------------- test data 1 ------------------------------ */
-// const markWeight = 78;
-// const markHeight = 1.69;
-// const johnWeight = 92;
-// const johnHeight = 1.95;
+const jonas = "I'm " + firstName + ', a ' + (year - birthYear) + ' year old ' + job + '!';
+console.log(jonas);
+/* --------------------------------- using $ -------------------------------- */
 
-/* ------------------------------- test data 2 ------------------------------ */
-const markWeight = 95;
-const markHeight = 1.88;
-const johnWeight = 85;
-const johnHeight = 1.76;
+const jonasNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`;
+console.log(jonasNew);
+console.log(`Just a regular string...`);
 
-/* --------------------------------- bmi formula -------------------------------- */
-const markBMI = markWeight / markHeight ** 2
-const johnBMI = johnWeight / johnHeight ** 2
+/* ----------------------------- multiple lines ----------------------------- */
+console.log('String with \n\
+multiple \n\
+lines');
 
-/* ------------------------------ comparing bmi ----------------------------- */
-if (markBMI > johnBMI) markHigherBMI1 = true
-else markHigherBMI1 = false
-/* ----------------------------------- or BETTER WAY ----------------------------------- */
-const markHigherBMI = markBMI > johnBMI
-console.log(markBMI, johnBMI, markHigherBMI)
+console.log(`String
+multiple
+lines`);
