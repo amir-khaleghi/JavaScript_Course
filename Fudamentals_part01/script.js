4f626b6abdb907a6e34762596a8ad50577419ded
amir-khaleghi <a.khaleghi.d@gmail.com>
2022-03-02 16:39:12 +0330

    Type Conversion and Coercion

--- Fudamentals_part01/script.js
+++ Fudamentals_part01/script.js
@@ -1,30 +1,21 @@
 /* -------------------------------------------------------------------------- */
-/*                             Coding Challenge #2                            */
+/*                        Type Conversion and Coercion•                       */
 /* -------------------------------------------------------------------------- */
+/* ----------------------------- type conversion ---------------------------- */
+const inputYear = '1991';
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+console.log(Number('Jonas'));
+console.log(typeof NaN);
+console.log(String(23), 23);
+/* ------------------------------ type coercion ----------------------------- */
+console.log('I am ' + 23 + ' years old');
+console.log('23' - '10' - 3);
+console.log('23' / '2');
 
-/*
-Use the BMI example from Challenge #1, and the code you already wrote, and improve it:
-1. Print a nice output to the console, saying who has the higher BMI. The message can be either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!"
-2. Use a template literal to include the BMI values in the outputs. Example: "Mark's BMI (28.3) is higher than John's (23.9)!"
-HINT: Use an if/else statement 😉
-GOOD LUCK 😀
-*/
-/* ------------------------------- data set  ------------------------------- */
-const massMark = 78;
-const heightMark = 1.69;
-const massJohn = 92;
-const heightJohn = 1.95;
-// const massMark = 95;
-// const heightMark = 1.88;
-// const massJohn = 85;
-// const heightJohn = 1.76;
-/* ----------------------------- bmi calculator ----------------------------- */
-const BMIMark = massMark / heightMark ** 2;
-const BMIJohn = massJohn / (heightJohn * heightJohn);
-console.log(BMIMark, BMIJohn);
-/* ------------------------------- comparison ------------------------------- */
-if (BMIMark > BMIJohn) {
-    console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`)
-} else {
-    console.log(`John's BMI (${BMIJohn}) is higher than Marks's (${BMIMark})!`)
-}
+let n = '1' + 1; // '11' 
+n = n - 1;
+console.log(n);
+console.log("2" - 1 + 5)
+console.log(1 - "2" + 5)
+console.log("2" + "2" - 1 + 5 + "2")
