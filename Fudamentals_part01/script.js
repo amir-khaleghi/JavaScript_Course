d7bed8cbd670b3ee9ce1f4cf79fe8654f03685de
amir-khaleghi <a.khaleghi.d@gmail.com>
2022-03-03 15:00:16 +0330

    Statements and Expressions

--- Fudamentals_part01/script.js
+++ Fudamentals_part01/script.js
@@ -1,73 +1,14 @@
 /* -------------------------------------------------------------------------- */
-/*                              The switch Statement                          */
+/*                         Statements and Expressions                         */
 /* -------------------------------------------------------------------------- */
+/* ------------------------------- expressions ------------------------------ */
+3 + 4
+1991
+true && false && !false
 
-const day = 'friday';
-switch (day) {
-    case 'monday': // day === 'monday'
-        console.log('Plan course structure');
-        console.log('Go to coding meetup');
-        break;
-    case 'tuesday':
-        console.log('Prepare theory videos');
-        break;
-    case 'wednesday':
-    case 'thursday':
-        console.log('Write code examples');
-        break;
-    case 'friday':
-        console.log('Record videos');
-        break;
-    case 'saturday':
-    case 'sunday':
-        console.log('Enjoy the weekend :D');
-        break;
-    default:
-        console.log('Not a valid day!');
+/* ------------------------------- statements ------------------------------- */
+if (23 > 10) {
+    const str = '23 is bigger';
 }
-if (day === 'monday') {
-    console.log('Plan course structure');
-    console.log('Go to coding meetup');
-} else if (day === 'tuesday') {
-    console.log('Prepare theory videos');
-} else if (day === 'wednesday' || day === 'thursday') {
-    console.log('Write code examples');
-} else if (day === 'friday') {
-    console.log('Record videos');
-} else if (day === 'saturday' || day === 'sunday') {
-    console.log('Enjoy the weekend :D');
-} else {
-    console.log('Not a valid day!');
-}
-/* --------------------------------- example -------------------------------- */
-
-// The getDay() method returns the weekday as a number between 0 and 6.
-
-// (Sunday=0, Monday=1, Tuesday=2 ..)
-
-// This example uses the weekday number to calculate the weekday name:
-
-switch (new Date().getDay()) {
-    case 0:
-        day = "Sunday";
-        break;
-    case 1:
-        day = "Monday";
-        break;
-    case 2:
-        day = "Tuesday";
-        break;
-    case 3:
-        day = "Wednesday";
-        break;
-    case 4:
-        day = "Thursday";
-        break;
-    case 5:
-        day = "Friday";
-        break;
-    case 6:
-        day = "Saturday";
-}
-const dayNumber = new Date().getDay()
-console.log(dayNumber)
+const me = 'Jonas';
+console.log(`I'm ${2037 - 1991} years old ${me}`); // ${add expressions here}
