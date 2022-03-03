9c2ddb3aed4d895cbcc8b8043a81a88f59f907f2
amir-khaleghi <a.khaleghi.d@gmail.com>
2022-03-03 14:54:09 +0330

    The switch Statement

--- Fudamentals_part01/script.js
+++ Fudamentals_part01/script.js
@@ -1,42 +1,73 @@
 /* -------------------------------------------------------------------------- */
-/*                             Coding Challenge #3                            */
+/*                              The switch Statement                          */
 /* -------------------------------------------------------------------------- */
 
-/*
-There are two gymnastics teams, Dolphins and Koalas. They compete against each other 3 times. The winner with the highest average score wins the a trophy!
-1. Calculate the average score for each team, using the test data below
-2. Compare the team's average scores to determine the winner of the competition, and print it to the console. Don't forget that there can be a draw, so test for that as well (draw means they have the same average score).
-3. BONUS 1: Include a requirement for a minimum score of 100. With this rule, a team only wins if it has a higher score than the other team, and the same time a score of at least 100 points. HINT: Use a logical operator to test for minimum score, as well as multiple else-if blocks 😉
-4. BONUS 2: Minimum score also applies to a draw! So a draw only happens when both teams have the same score and both have a score greater or equal 100 points. Otherwise, no team wins the trophy.
-TEST DATA: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
-TEST DATA BONUS 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
-TEST DATA BONUS 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
-GOOD LUCK 😀
-*/
-
-/* --------------------------------- dataset -------------------------------- */
-const dolphinsScore = (96 + 108 + 89) / 3;
-const KoalasScore = (88 + 91 + 110) / 3;
-console.log(KoalasScore, dolphinsScore)
-/* --------------------------------- compare -------------------------------- */
-// if (dolphinsScore === KoalasScore) {
-//     console.log("Draw!")
-// } else if (KoalasScore < dolphinsScore) {
-//     console.log("Dolphins win!")
-// } else {
-//     console.log("Koalas win!")
-// }
-
-/* --------------------------------- BONUS 1 -------------------------------- */
-
-if (dolphinsScore >= 100 || KoalasScore >= 100) {
-    if (dolphinsScore > KoalasScore) {
-        console.log("Dolphins win!")
-    } else if (KoalasScore > dolphinsScore) {
-        console.log("Koalas win!")
-    } else if (KoalasScore === dolphinsScore) {
-        console.log("Draw!")
-    }
-} else {
-    console.log("No one win!")
+const day = 'friday';
+switch (day) {
+    case 'monday': // day === 'monday'
+        console.log('Plan course structure');
+        console.log('Go to coding meetup');
+        break;
+    case 'tuesday':
+        console.log('Prepare theory videos');
+        break;
+    case 'wednesday':
+    case 'thursday':
+        console.log('Write code examples');
+        break;
+    case 'friday':
+        console.log('Record videos');
+        break;
+    case 'saturday':
+    case 'sunday':
+        console.log('Enjoy the weekend :D');
+        break;
+    default:
+        console.log('Not a valid day!');
 }
+if (day === 'monday') {
+    console.log('Plan course structure');
+    console.log('Go to coding meetup');
+} else if (day === 'tuesday') {
+    console.log('Prepare theory videos');
+} else if (day === 'wednesday' || day === 'thursday') {
+    console.log('Write code examples');
+} else if (day === 'friday') {
+    console.log('Record videos');
+} else if (day === 'saturday' || day === 'sunday') {
+    console.log('Enjoy the weekend :D');
+} else {
+    console.log('Not a valid day!');
+}
+/* --------------------------------- example -------------------------------- */
+
+// The getDay() method returns the weekday as a number between 0 and 6.
+
+// (Sunday=0, Monday=1, Tuesday=2 ..)
+
+// This example uses the weekday number to calculate the weekday name:
+
+switch (new Date().getDay()) {
+    case 0:
+        day = "Sunday";
+        break;
+    case 1:
+        day = "Monday";
+        break;
+    case 2:
+        day = "Tuesday";
+        break;
+    case 3:
+        day = "Wednesday";
+        break;
+    case 4:
+        day = "Thursday";
+        break;
+    case 5:
+        day = "Friday";
+        break;
+    case 6:
+        day = "Saturday";
+}
+const dayNumber = new Date().getDay()
+console.log(dayNumber)
