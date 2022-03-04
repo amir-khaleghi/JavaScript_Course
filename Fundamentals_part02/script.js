a99d1b6989c4fc600f797d8368d1023ab6fdc65b
amir-khaleghi <a.khaleghi.d@gmail.com>
2022-03-04 09:51:31 +0330

    Functions

--- Fundamentals_part02/script.js
+++ Fundamentals_part02/script.js
@@ -1,12 +1,27 @@
 /* -------------------------------------------------------------------------- */
-/*                           Activating Strict Mode                           */
+/*                                  Functions                                 */
 /* -------------------------------------------------------------------------- */
 
-'use strict';
+function logger() {
+    ""
+    " description
+    ""
+    "
+    console.log('My name is Jonas');
+}
+// calling / running / invoking function
+logger();
+logger();
+logger();
 
-let hasDriversLicense = false;
-const passTest = true;
-if (passTest) hasDriversLicense = true;
-if (hasDriversLicense) console.log('I can drive :D');
-// const interface = 'Audio';
-// const private = 534;
+function fruitProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+const num = Number('23');
+
+Number("10")
