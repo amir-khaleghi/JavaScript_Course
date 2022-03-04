6ffcd53ea6f46ed79284df58fb9b10c4aec4c7c0
amir-khaleghi <a.khaleghi.d@gmail.com>
2022-03-04 10:47:14 +0330

    Arrow functions

--- Fundamentals_part02/script.js
+++ Fundamentals_part02/script.js
@@ -1,14 +1,15 @@
 /* -------------------------------------------------------------------------- */
-/*                    Function Declarations vs. Expressions                   */
+/*                               Arrow functions                              */
 /* -------------------------------------------------------------------------- */
-// Function declaration
-function calcAge1(birthYeah) {
-    return 2037 - birthYeah;
+
+const calcAge3 = birthYeah => 2037 - birthYeah;
+const age3 = calcAge3(1991);
+console.log(age3);
+const yearsUntilRetirement = (birthYeah, firstName) => {
+    const age = 2037 - birthYeah;
+    const retirement = 65 - age;
+    // return retirement;
+    return `${firstName} retires in ${retirement} years`;
 }
-const age1 = calcAge1(1991);
-// Function expression
-const calcAge2 = function (birthYeah) {
-    return 2037 - birthYeah;
-}
-const age2 = calcAge2(1991);
-console.log(age1, age2);
+console.log(yearsUntilRetirement(1991, 'Jonas'));
+console.log(yearsUntilRetirement(1980, 'Bob'));
