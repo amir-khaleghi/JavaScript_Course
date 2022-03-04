24ad19c60db43cc042ad499c398586c33f180560
amir-khaleghi <a.khaleghi.d@gmail.com>
2022-03-04 10:10:03 +0330

    Function Declarations vs. Expressions

--- Fundamentals_part02/script.js
+++ Fundamentals_part02/script.js
@@ -1,27 +1,14 @@
 /* -------------------------------------------------------------------------- */
-/*                                  Functions                                 */
+/*                    Function Declarations vs. Expressions                   */
 /* -------------------------------------------------------------------------- */
-
-function logger() {
-    ""
-    " description
-    ""
-    "
-    console.log('My name is Jonas');
+// Function declaration
+function calcAge1(birthYeah) {
+    return 2037 - birthYeah;
 }
-// calling / running / invoking function
-logger();
-logger();
-logger();
-
-function fruitProcessor(apples, oranges) {
-    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
-    return juice;
+const age1 = calcAge1(1991);
+// Function expression
+const calcAge2 = function (birthYeah) {
+    return 2037 - birthYeah;
 }
-const appleJuice = fruitProcessor(5, 0);
-console.log(appleJuice);
-const appleOrangeJuice = fruitProcessor(2, 4);
-console.log(appleOrangeJuice);
-const num = Number('23');
-
-Number("10")
+const age2 = calcAge2(1991);
+console.log(age1, age2);
