4fc9e5a016bacc2c753a9a9bed1726b3929d2950
amir-khaleghi <a.khaleghi.d@gmail.com>
2022-03-04 11:03:49 +0330

    Reviewing Functions

--- Fundamentals_part02/script.js
+++ Fundamentals_part02/script.js
@@ -1,14 +1,19 @@
 /* -------------------------------------------------------------------------- */
-/*                      Functions Calling Other Functions                     */
+/*                             Reviewing Functions                            */
 /* -------------------------------------------------------------------------- */
-function cutFruitPieces(fruit) {
-    return fruit * 4;
+const calcAge = function (birthYeah) {
+    return 2037 - birthYeah;
 }
-
-function fruitProcessor(apples, oranges) {
-    const applePieces = cutFruitPieces(apples);
-    const orangePieces = cutFruitPieces(oranges);
-    const juice = `Juice with ${applePieces} piece of apple and ${orangePieces} pieces of orange.`;
-    return juice;
+const yearsUntilRetirement = function (birthYeah, firstName) {
+    const age = calcAge(birthYeah);
+    const retirement = 65 - age;
+    if (retirement > 0) {
+        console.log(`${firstName} retires in ${retirement} years`);
+        return retirement;
+    } else {
+        console.log(`${firstName} has already retired 🎉`);
+        return -1;
+    }
 }
-console.log(fruitProcessor(2, 3));
+console.log(yearsUntilRetirement(1991, 'Jonas'));
+console.log(yearsUntilRetirement(1950, 'Mike'));
