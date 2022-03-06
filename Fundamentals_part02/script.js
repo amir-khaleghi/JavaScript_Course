f2b94b485d47550b12d8fbc694fc3e66892a061f
amir-khaleghi <a.khaleghi.d@gmail.com>
2022-03-06 19:04:54 +0330

    Coding Challenge #4

--- Fundamentals_part02/script.js
+++ Fundamentals_part02/script.js
@@ -1,48 +1,85 @@
 /* -------------------------------------------------------------------------- */
-/*                   Looping Arrays, Breaking and Continuing                  */
+/*                             Coding Challenge #4                            */
 /* -------------------------------------------------------------------------- */
-/* ---------------------------------- array --------------------------------- */
-const jonas = [
-    'Jonas',
-    'Schmedtmann',
-    2037 - 1991,
-    'teacher',
-    ['Michael', 'Peter', 'Steven'],
-    true
-];
-const types = [];
-// console.log(jonas[0])
-// console.log(jonas[1])
-// ...
-// console.log(jonas[4])
-// jonas[5] does NOT exist
+/*
+Let's improve Steven's tip calculator even more, this time using loops!
+1. Create an array 'bills' containing all 10 test bill values
+2. Create empty arrays for the tips and the totals ('tips' and 'totals')
+3. Use the 'calcTip' function we wrote before (no need to repeat) to calculate tips and total values (bill + tip) for every bill value in the bills array. Use a for loop to perform the 10 calculations!
+TEST DATA: 22, 295, 176, 440, 37, 105, 10, 1100, 86 and 52
+HINT: Call calcTip in the loop and use the push method to add values to the tips and totals arrays 😉
+4. BONUS: Write a function 'calcAverage' which takes an array called 'arr' as an argument. This function calculates the average of all numbers in the given array. This is a DIFFICULT challenge (we haven't done this before)! Here is how to solve it:
+  4.1. First, you will need to add up all values in the array. To do the addition, start by creating a variable 'sum' that starts at 0. Then loop over the array using a for loop. In each iteration, add the current value to the 'sum' variable. This way, by the end of the loop, you have all values added together
+  4.2. To calculate the average, divide the sum you calculated before by the length of the array (because that's the number of elements)
+  4.3. Call the function with the 'totals' array
+GOOD LUCK 😀
+*/
+/* ------------------------------------ 1 ----------------------------------- */
+const bill = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+/* ------------------------------------ 2 ----------------------------------- */
+const tip = [];
+const totals = [];
+/* ------------------------------------ 3 ----------------------------------- */
+for (let i = 0; i < bill.length; i++) {
+    calcTip = (i) => {
+        tip.push(bill[i] * 0.2)
+        totals.push(bill[i] + tip[i]);
+    }
+    calcTip(i)
+}
+console.log(tip)
+console.log(totals)
+/* ------------------------------------ 4 ----------------------------------- */
+/* ---------------------------- average function ---------------------------- */
+const calcAverage = function (arr) {
+    let sum = 0
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i]
+    }
+    return sum / arr.length
+}
+console.log(calcAverage([1, 3, 4, 4]))
+console.log(calcAverage(totals))
+console.log(calcAverage(tip))
 
-for (let i = 0; i < jonas.length; i++) {
-    /* ------------------------ Reading from jonas array ------------------------ */
-    console.log(jonas[i], typeof jonas[i]);
-    /* --------------------------- Filling types array -------------------------- */
-    // types[i] = typeof jonas[i];
-    types.push(typeof jonas[i]);
-}
-console.log(types);
-/* ------------------------------ years and age ----------------------------- */
-const years = [1991, 2007, 1969, 2020];
-const ages = [];
-for (let i = 0; i < years.length; i++) {
-    ages.push(2037 - years[i]);
-}
-console.log(ages);
-/* ------------------------------- for and if ------------------------------- */
 
-/* -------------------------------- continue -------------------------------- */
-console.log('--- ONLY STRINGS ---')
-for (let i = 0; i < jonas.length; i++) {
-    if (typeof jonas[i] !== 'string') continue;
-    console.log(jonas[i], typeof jonas[i]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 }
-/* ---------------------------------- break --------------------------------- */
-console.log('--- BREAK WITH NUMBER ---')
-for (let i = 0; i < jonas.length; i++) {
-    if (typeof jonas[i] === 'number') break;
-    console.log(jonas[i], typeof jonas[i]);
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+for (let i = 0; i < bills.length; i++) {
+  const tip = calcTip(bills[i]);
+  tips.push(tip);
+  totals.push(tip + bills[i]);
 }
+console.log(bills, tips, totals);
+const calcAverage = function (arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    // sum = sum + arr[i];
+    sum += arr[i];
+  }
+  return sum / arr.length;
+}
+console.log(calcAverage([2, 3, 7]));
+console.log(calcAverage(totals));
+console.log(calcAverage(tips));
+*/
