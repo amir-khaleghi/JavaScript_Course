38f7209b26e042880b937e47d1f48d9a120f98e2
amir-khaleghi <a.khaleghi.d@gmail.com>
2022-03-07 19:37:34 +0330

    Run Nodejs Server

---
+++ 5_Developer Skills & Editor Setup/script.js
@@ -0,0 +1,11 @@
+/* ------------------------- installing new packages ------------------------ */
+// 1. prettier
+// 2.live server
+// 3.to do highlight
+
+/* --------------------------- installing Node.js --------------------------- */
+// check its version with: node--version
+// install packages: npm install live-server -g
+
+/* --------------------------- run node js server --------------------------- */
+// liver-server  in terminal
