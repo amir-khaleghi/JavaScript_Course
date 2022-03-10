390669e772aa5b72728a8d89075fe483c3b1d7f9
amir-khaleghi <a.khaleghi.d@gmail.com>
2022-03-10 11:22:03 +0330

    Implementing Highscores

--- 07_BrowserDOM and Events Fundamentals/script.js
+++ 07_BrowserDOM and Events Fundamentals/script.js
@@ -5,14 +5,17 @@
 /* ------------------------------ initialState ------------------------------ */
 // const initDoc = $.extend({}, document);
 
-/* -------------------------- generate guess number ------------------------- */
+/* ---------------------------- initial variables --------------------------- */
+//  generate guess number 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 // document.querySelector('.number').textContent = "?";
-
-/* ----------------------------- generate score ----------------------------- */
+// score number
 let score = 20;
+// highscore
+let highScore = 0;
 
-/* ----------- getring guess number and compare with secret number ---------- */
+
+/* ----------- generating guess number and compare with secret number ---------- */
 document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
     // when there is no input
@@ -23,11 +26,20 @@ document.querySelector('.check').addEventListener('click', function () {
     } else if (guess === secretNumber) {
         document.querySelector('.message').textContent = 'You Won!';
         document.querySelector('.number').textContent = secretNumber;
-        /* ----------------------------- backgroundColor ---------------------------- */
+
+        // backgroundColor
         document.body.style.background = '#60b347';
         // document.querySelector('body').style.backgroundColor = '#60b347';
-        /* ------------------------------ change width ------------------------------ */
+
+        // change width of score number
         document.querySelector('.number').style.width = '40rem';
+
+        // insert highscore
+        if (score > highScore) {
+            document.querySelector(".highscore").textContent = score;
+            highScore = score;
+        }
+
         // when guess number is higher
     } else if (guess > secretNumber) {
         // score -= 1
@@ -40,7 +52,7 @@ document.querySelector('.check').addEventListener('click', function () {
     }
 
     /* ------------------------ manipulating score number ----------------------- */
-    document.querySelector('.label-score').textContent = score;
+    document.querySelector('.score').textContent = score;
     // when score = 0 and player lost
     if (score === 0) {
         document.querySelector('.message').textContent = 'You lost the game!';
@@ -54,7 +66,7 @@ document.querySelector('.again').addEventListener('click', function () {
 
     document.querySelector('.message').textContent = 'Start guessing...';
     // displayMessage('Start guessing...');
-    document.querySelector('.label-score').textContent = "💯 Score: 20";
+    document.querySelector('.score').textContent = "20";
     document.querySelector('.number').textContent = '?';
     document.querySelector('.guess').value = '';
 
