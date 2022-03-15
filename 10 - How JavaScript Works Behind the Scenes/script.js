3f842776c99305278a536542f72794864e4197a9
amir-khaleghi <a.khaleghi.d@gmail.com>
2022-03-15 13:24:03 +0330

    Objects vs. primitives

--- 10 - How JavaScript Works Behind the Scenes/script.js
+++ 10 - How JavaScript Works Behind the Scenes/script.js
@@ -1,45 +1,17 @@
 /* -------------------------------------------------------------------------- */
-/*                    Regular Functions vs. Arrow Functions                   */
+/*                           Objects vs. primitives                           */
 /* -------------------------------------------------------------------------- */
-// var firstName = 'Matilda';
-const jonas = {
-    firstName: 'Jonas',
-    year: 1991,
-    calcAge: function () {
-        // console.log(this);
-        console.log(2037 - this.year);
-        /* ------------------------------- Solution 1 ------------------------------- */
-        // const self = this; // self or that
-        // const isMillenial = function () {
-        //   console.log(self);
-        //   console.log(self.year >= 1981 && self.year <= 1996);
-        // };
-        /* ------------------------------- Solution 2 ------------------------------- */
-        const isMillenial = () => {
-            console.log(this);
-            console.log(this.year >= 1981 && this.year <= 1996);
-        };
-        isMillenial();
-    },
-    greet: () => {
-        console.log(this);
-        console.log(`Hey ${this.firstName}`);
-    },
-};
-
-jonas.greet();
-jonas.calcAge();
+let age = 30;
+let oldAge = age;
+age = 31;
+console.log(age); //31
+console.log(oldAge); //30
 // ────────────────────────────────────────────────────────────────────────────────
-// arguments keyword
-// ────────────────────────────────────────────────────────────────────────────────
-const addExpr = function (a, b) {
-    console.log(arguments);
-    return a + b;
+const me = {
+    name: 'Jonas',
+    age: 30,
 };
-addExpr(2, 5);
-addExpr(2, 5, 8, 12);
-var addArrow = (a, b) => {
-    console.log(arguments);
-    return a + b;
-};
-addArrow(2, 5, 8);
+const friend = me;
+friend.age = 27;
+console.log('Friend:', friend); //27
+console.log('Me', me); //27
