22ae45a992500e0a5d47f834e1cda1e6a6ee6a64
amir-khaleghi <a.khaleghi.d@gmail.com>
2022-03-15 15:06:10 +0330

    Primitives vs. Objects in Practice

--- 10 - How JavaScript Works Behind the Scenes/script.js
+++ 10 - How JavaScript Works Behind the Scenes/script.js
@@ -1,17 +1,34 @@
 /* -------------------------------------------------------------------------- */
-/*                           Objects vs. primitives                           */
+/*                     Primitives vs. Objects in Practice                     */
 /* -------------------------------------------------------------------------- */
-let age = 30;
-let oldAge = age;
-age = 31;
-console.log(age); //31
-console.log(oldAge); //30
-// ────────────────────────────────────────────────────────────────────────────────
-const me = {
-    name: 'Jonas',
-    age: 30,
+
+/* ----------------------------- Primitive types ---------------------------- */
+let lastName = 'Williams';
+let oldLastName = lastName;
+lastName = 'Davis';
+console.log(lastName, oldLastName);
+/* ----------------------------- Reference types ---------------------------- */
+const jessica = {
+    firstName: 'Jessica',
+    lastName: 'Williams',
+    age: 27,
 };
-const friend = me;
-friend.age = 27;
-console.log('Friend:', friend); //27
-console.log('Me', me); //27
+const marriedJessica = jessica;
+marriedJessica.lastName = 'Davis';
+console.log('Before marriage:', jessica);
+console.log('After marriage: ', marriedJessica);
+// marriedJessica = {};
+/* ----------------------------- Copying objects ---------------------------- */
+const jessica2 = {
+    firstName: 'Jessica',
+    lastName: 'Williams',
+    age: 27,
+    family: ['Alice', 'Bob'],
+};
+// ────────────────────────────────────────────────────────────────────────────────
+const jessicaCopy = Object.assign({}, jessica2);
+jessicaCopy.lastName = 'Davis';
+jessicaCopy.family.push('Mary');
+jessicaCopy.family.push('John');
+console.log('Before marriage:', jessica2);
+console.log('After marriage: ', jessicaCopy);
