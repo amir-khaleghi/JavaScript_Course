47b2f10e59b79bdeab1216bb893404b190f322ba
amir-khaleghi <a.khaleghi.d@gmail.com>
2022-03-18 15:32:37 +0330

    Destructuring Objects

--- 11 - Data Structures, Modern Operators and Strings/script.js
+++ 11 - Data Structures, Modern Operators and Strings/script.js
@@ -1,26 +1,16 @@
-'use strict';
-// ────────────────────────────────────────────────────────────────────────────────
-/* -------------------------------------------------------------------------- */
-/*                            Destructuring Arrays                            */
-/* -------------------------------------------------------------------------- */
-// ────────────────────────────────────────────────────────────────────────────────
-
+"use strict";
 /* -------------------- Data needed for a later exercise -------------------- */
 const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+  "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
 
+// ────────────────────────────────────────────────────────────────────────────────
 /* ---------------- Data needed for first part of the section --------------- */
 const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-  /* ----------------------------- create function ---------------------------- */
-  order: function (starterIndex, mainIndex) {
-    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]]
-  },
-
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
   openingHours: {
     thu: {
       open: 12,
@@ -35,31 +25,70 @@ const restaurant = {
       close: 24,
     },
   },
+  /* ----------------------------- create function ---------------------------- */
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery: function ({
+    starterIndex = 1, //default values
+    mainIndex = 0,
+    time = "20:00",
+    address,
+  }) {
+    console.log(
+      `Order Recieved! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
 };
 // ────────────────────────────────────────────────────────────────────────────────
-const arr = [2, 3, 4];
-const a = arr[0];
-const b = arr[1];
-const c = arr[2];
+/* ----------------------------- Calling Method ----------------------------- */
+restaurant.orderDelivery({
+  time: "22:30",
+  address: "Via del sole, 21",
+  mainIndex: 2,
+  starterIndex: 2,
+});
 // ────────────────────────────────────────────────────────────────────────────────
-const [x, y, z] = arr;
-console.log(x, y, z);
-console.log(arr);
+/* -------------------------------------------------------------------------- */
+/*                            Destructuring Objects                           */
+/* -------------------------------------------------------------------------- */
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
 // ────────────────────────────────────────────────────────────────────────────────
-// categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-// starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-// mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-let [main, secondary] = restaurant.categories;
-console.log(main, secondary);
 // ────────────────────────────────────────────────────────────────────────────────
-// const temp = main;
-// main = secondary;
-// secondary = temp;
-// console.log(main, secondary);
-/* ----------------------------------- or ----------------------------------- */
-[main, secondary] = [secondary, main]
-console.log(main, secondary)
-/* ----------------------------- order function ----------------------------- */
-console.log(restaurant.order(2, 0))
-console.log(restaurant.order(0, 2))
-console.log(restaurant.order(0, 0))
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+console.log(restaurantName, hours, tags);
+// ────────────────────────────────────────────────────────────────────────────────
+// setting defaults
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
+// ────────────────────────────────────────────────────────────────────────────────
+/* --------------------------- mutating variables --------------------------- */
+let a = 111;
+let b = 999;
+const obj = {
+  a: 23,
+  b: 7,
+  c: 14,
+};
+
+/* ---------------------------- USING PARANTHESIS --------------------------- */
+({ a, b } = obj);
+
+/* ----------------------------- nested objects ----------------------------- */
+const {
+  sat: { open, close },
+} = openingHours;
+console.log(open, close);
+// const {
+// fri: { open, close },  // can't use identifier
+// } = openingHours;
+const {
+  sat: { open: o, close: c },
+} = openingHours;
+console.log(o, c);
