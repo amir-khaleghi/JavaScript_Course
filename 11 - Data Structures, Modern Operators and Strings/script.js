8e3456be6e900acaa7f8e207d84ad98c28ea2143
amir-khaleghi <a.khaleghi.d@gmail.com>
2022-03-22 20:29:47 +0430

    Short Circuiting (&& and ||)

--- 11 - Data Structures, Modern Operators and Strings/script.js
+++ 11 - Data Structures, Modern Operators and Strings/script.js
@@ -58,37 +58,31 @@ restaurant.orderDelivery({
   mainIndex: 2,
   starterIndex: 2,
 });
-
 /* -------------------------------------------------------------------------- */
-/*                         Rest Pattern and Parameters                        */
+/*                        Short Circuiting (&& and ||)                        */
 /* -------------------------------------------------------------------------- */
-// 1) Destructuring
-/* ------------------- SPREAD, because on RIGHT side of = ------------------- */
-const arr = [1, 2, ...[3, 4]];
-
-/* --------------------- REST, because on LEFT side of = -------------------- */
-const [a, b, ...others] = [1, 2, 3, 4, 5];
-console.log(a, b, others);
-const [pizza, , risotto, ...otherFood] = [
-  ...restaurant.mainMenu,
-  ...restaurant.starterMenu,
-];
-console.log(pizza, risotto, otherFood);
-
-/* --------------------------------- Objects -------------------------------- */
-const { sat, ...weekdays } = restaurant.openingHours;
-console.log(weekdays);
-
-/* ------------------------------ 2) Functions ------------------------------ */
-const add = function (...numbers) {
-  let sum = 0;
-  for (let i = 0; i < numbers.length; i++) sum += numbers[i];
-  console.log(sum);
-};
-add(2, 3);
-add(5, 3, 7, 2);
-add(8, 2, 5, 3, 2, 1, 4);
-const x = [23, 5, 7];
-add(...x);
-restaurant.orderPizza("mushrooms", "onion", "olives", "spinach");
-restaurant.orderPizza("mushrooms");
+console.log('---- OR ----');
+// Use ANY data type, return ANY data type, short-circuiting
+console.log(3 || 'Jonas');
+console.log('' || 'Jonas');
+console.log(true || 0);
+console.log(undefined || null);
+console.log(undefined || 0 || '' || 'Hello' || 23 || null);
+// ────────────────────────────────────────────────────────────────────────────────
+restaurant.numGuests = 0;
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10; //to check if it exist 
+console.log(guests1);
+// ────────────────────────────────────────────────────────────────────────────────
+const guests2 = restaurant.numGuests || 10;
+console.log(guests2);
+console.log('---- AND ----');
+console.log(0 && 'Jonas');
+console.log(7 && 'Jonas');
+console.log('Hello' && 23 && null && 'jonas');
+// ────────────────────────────────────────────────────────────────────────────────
+// Practical example to check existence of a value and property
+if (restaurant.orderPizza) {
+  restaurant.orderPizza('mushrooms', 'spinach');
+}
+//short circuit if restaurant.orderPizza doesn't exist.
+restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
