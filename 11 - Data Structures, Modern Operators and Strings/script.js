aa9a1c44e9628725ca8b22a75ab4bd3fbdd2836d
amir-khaleghi <a.khaleghi.d@gmail.com>
2022-03-22 21:02:46 +0430

    The Nullish Coalescing Operator

--- 11 - Data Structures, Modern Operators and Strings/script.js
+++ 11 - Data Structures, Modern Operators and Strings/script.js
@@ -59,32 +59,11 @@ restaurant.orderDelivery({
   starterIndex: 2,
 });
 /* -------------------------------------------------------------------------- */
-/*                        Short Circuiting (&& and ||)                        */
+/*                       The Nullish Coalescing Operator                      */
 /* -------------------------------------------------------------------------- */
-console.log('---- OR ----');
-// Use ANY data type, return ANY data type, short-circuiting
-
-
-console.log(3 || 'Jonas');
-console.log('' || 'Jonas');
-console.log(true || 0);
-console.log(undefined || null);
-console.log(undefined || 0 || '' || 'Hello' || 23 || null);
-// ────────────────────────────────────────────────────────────────────────────────
 restaurant.numGuests = 0;
-const guests1 = restaurant.numGuests ? restaurant.numGuests : 10; //to check if it exist 
-console.log(guests1);
-// ────────────────────────────────────────────────────────────────────────────────
-const guests2 = restaurant.numGuests || 10;
-console.log(guests2);
-console.log('---- AND ----');
-console.log(0 && 'Jonas');
-console.log(7 && 'Jonas');
-console.log('Hello' && 23 && null && 'jonas');
-// ────────────────────────────────────────────────────────────────────────────────
-// Practical example to check existence of a value and property
-if (restaurant.orderPizza) {
-  restaurant.orderPizza('mushrooms', 'spinach');
-}
-//short circuit if restaurant.orderPizza doesn't exist.
-restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
+const guests = restaurant.numGuests || 10 ;
+console.log(guests);
+// Nullish: null and undefined (NOT 0 or '')
+const guestCorrect = restaurant.numGuests ?? 10;
+console.log(guestCorrect);
