6f2c6999baa73980c95fa8ad18657482c5b9adc4
amir-khaleghi <a.khaleghi.d@gmail.com>
2022-03-26 10:07:12 +0430

    Coding Challenge #1

--- 11 - Data Structures, Modern Operators and Strings/script.js
+++ 11 - Data Structures, Modern Operators and Strings/script.js
@@ -1,90 +1,114 @@
 "use strict";
-/* -------------------- Data needed for a later exercise -------------------- */
-const flights =
-  "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
 
-// ────────────────────────────────────────────────────────────────────────────────
-/* ---------------- Data needed for first part of the section --------------- */
-const restaurant = {
-  name: "Classico Italiano",
-  location: "Via Angelo Tavanti 23, Firenze, Italy",
-  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
-  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
-  mainMenu: ["Pizza", "Pasta", "Risotto"],
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
-  /* ----------------------------- create function ---------------------------- */
-  order: function (starterIndex, mainIndex) {
-    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-  },
-
-  orderDelivery: function ({
-    starterIndex = 1, //default values
-    mainIndex = 0,
-    time = "20:00",
-    address,
-  }) {
-    console.log(
-      `Order Recieved! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
-    );
-  },
-  orderPasta: function (ing1, ing2, ing3) {
-    console.log(
-      `Here is your delicious pasta with ${ing1}, ${ing2}, and ${ing3}`
-    );
-  },
-  orderPizza: function (mainIngredient, ...otherIngredient) {
-    console.log(mainIngredient);
-    console.log(otherIngredient);
-  },
-};
-// ────────────────────────────────────────────────────────────────────────────────
 /* -------------------------------------------------------------------------- */
-/*                        Logical Assignment Operators                        */
+/*                             Coding Challenge #1                            */
 /* -------------------------------------------------------------------------- */
-// ────────────────────────────────────────────────────────────────────────────────
-const rest1 = {
-  name: "Capri",
-  numGuests: 20,
+
+/* 
+We're building a football betting app (soccer for my American friends 😅)!
+Suppose we get data from a web service about a certain game (below). In this challenge we're gonna work with the data. So here are your tasks:
+
+GOOD LUCK 😀
+*/
+const game = {
+  team1: "Bayern Munich",
+  team2: "Borrussia Dortmund",
+  players: [
+    [
+      "Neuer",
+      "Pavard",
+      "Martinez",
+      "Alaba",
+      "Davies",
+      "Kimmich",
+      "Goretzka",
+      "Coman",
+      "Muller",
+      "Gnarby",
+      "Lewandowski",
+    ],
+    [
+      "Burki",
+      "Schulz",
+      "Hummels",
+      "Akanji",
+      "Hakimi",
+      "Weigl",
+      "Witsel",
+      "Hazard",
+      "Brandt",
+      "Sancho",
+      "Gotze",
+    ],
+  ],
+  score: "4:0",
+  scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
+  date: "Nov 9th, 2037",
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
 };
-
-const rest2 = {
-  name: "Amir",
-  owner: "Elahi",
-};
-
-/* ------------------------- or Assignment operator ------------------------- */
-rest1.numGuest = rest1.numGuests || 10;
-console.log("🚀 ~ file: script.js ~ line 69 ~ rest1", rest1);
-rest2.numGuest = rest2.numGuests || 10;
-console.log("🚀 ~ file: script.js ~ line 71 ~ rest2", rest2);
 // ────────────────────────────────────────────────────────────────────────────────
-rest1.numGuests ||= 10;
-rest2.numGuest ||= 10;
-console.log("🚀 ~ file: script.js ~ line 76 ~ rest2", rest2);
-console.log("🚀 ~ file: script.js ~ line 69 ~ rest1", rest1);
+// 1. Create one player array for each team (variables 'players1' and 'players2')
+// ────────────────────────────────────────────────────────────────────────────────
+const [players1, players2] = game.players;
+console.log(
+  "🚀 ~ file: script.js ~ line 62 ~ players1,players2",
+  players1,
+  players2
+);
+// ────────────────────────────────────────────────────────────────────────────────
+// 2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
+// ────────────────────────────────────────────────────────────────────────────────
+const [gk, ...fieldPlayers] = players1;
+console.log(
+  "🚀 ~ file: script.js ~ line 67 ~ gk,...fieldPlayers",
+  gk,
+  ...fieldPlayers
+);
+// ────────────────────────────────────────────────────────────────────────────────
+// 3. Create an array 'allPlayers' containing all players of both teams (22 players)
+// ────────────────────────────────────────────────────────────────────────────────
+const allPlayers = [...players1, ...players2];
+console.log("🚀 ~ file: script.js ~ line 81 ~ allPlayers", allPlayers)
 
-/* ------------------- nullish assignment operator(null or undefined ) ------------------- */
-rest1.numGuests ??= 10;
-rest2.numGuest ??= 10;
+// ────────────────────────────────────────────────────────────────────────────────
+// 4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+// ────────────────────────────────────────────────────────────────────────────────
+const players1Final = [...players1,'Thiago','Coutinho','Perisic'];
+console.log("🚀 ~ file: script.js ~ line 87 ~ players1Final", players1Final)
 
-/* ------------------------- and assignment operator ------------------------ */
-// rest1.owner = rest1.owner && '<ANONYMOUS';
-// rest2.owner = rest2.owner && '<ANONYMOUS';
 
-rest1.numGuest &&= "<ANONYMOUS";
-console.log("🚀 ~ file: script.js ~ line 88 ~ rest1", rest1)
-rest2.numGuest &&= "<ANONYMOUS";
-console.log("🚀 ~ file: script.js ~ line 90 ~ rest2", rest2)
+
+// ────────────────────────────────────────────────────────────────────────────────
+// 5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+// ────────────────────────────────────────────────────────────────────────────────
+const {
+  odds: { team1, x: draw, team2 },
+} = game;
+console.log(team1, draw, team2);
+console.log(game.odds)
+
+
+
+// ────────────────────────────────────────────────────────────────────────────────
+// 6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
+// ────────────────────────────────────────────────────────────────────────────────
+const printGoals = function (...players) {
+  console.log(players);
+  console.log(`${players.length} goals were scored`);
+};
+// printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+// printGoals('Davies', 'Muller');
+printGoals(...game.scored);
+
+
+
+// ────────────────────────────────────────────────────────────────────────────────
+// 7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
+// TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
+// ────────────────────────────────────────────────────────────────────────────────
+team1 < team2 && console.log('Team 1 is more likely to win');
+team1 > team2 && console.log('Team 2 is more likely to win');
