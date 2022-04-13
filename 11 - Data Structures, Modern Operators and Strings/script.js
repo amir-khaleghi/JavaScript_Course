6545a8c830a2a9eafca54afc8c67ed740d7970ba
amir-khaleghi <a.khaleghi.d@gmail.com>
2022-04-13 13:18:42 +0430

    Challenge 4

--- 11 - Data Structures, Modern Operators and Strings/script.js
+++ 11 - Data Structures, Modern Operators and Strings/script.js
@@ -43,10 +43,11 @@ const restaurant = {
 // ────────────────────────────────────────────────────────────────────────────────
 // ────────────────────────────────────────────────────────────────────────────────
 
+//ANCHOR String Methods Practice
+
 //ANCHOR Challenge 4
-/* -------------------------------------------------------------------------- */
-/*                             Coding Challenge #4                            */
-/* -------------------------------------------------------------------------- */
+// ────────────────────────────────────────────────────────────────────────────────
+
 /* 
 Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
 The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
@@ -63,51 +64,34 @@ someVariable        ✅✅✅
 calculateAge        ✅✅✅✅
 delayedDeparture    ✅✅✅✅✅
 */
-document.body.append(document.createElement('textarea'));
-document.body.append(document.createElement('button'));
-document.querySelector('button').addEventListener('click', function () { 
-  const text = document.querySelector('textarea').value;
-
+/*
+// ────────────────────────────────────────────────────────────────────────────────
+//NOTE create a new element in body of html
+// ────────────────────────────────────────────────────────────────────────────────
+document.body.append(document.createElement("textarea"));
+document.body.append(document.createElement("button"));
+// ────────────────────────────────────────────────────────────────────────────────
 // HINT 1: Remember which character defines a new line in the textarea 😉
-
-
-
-
-
-/*
-HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
-HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
-HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
-Afterwards, test with your own test data!
-GOOD LUCK 😀
-*/
-
-/*
-document.body.append(document.createElement('textarea'));
-document.body.append(document.createElement('button'));
-document.querySelector('button').addEventListener('click', function () { 
-  const text = document.querySelector('textarea').value;
-
-
-
-  const rows = text.split('\n');
+//NOTE get input from textarea
+// ────────────────────────────────────────────────────────────────────────────────
+document.querySelector("button").addEventListener("click", function () {
+  const text = document.querySelector("textarea").value;
+//NOTE creat an array of inputs by seperating them
+// ────────────────────────────────────────────────────────────────────────────────
+  const rows = text.split("\n");
+  // HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
+// ────────────────────────────────────────────────────────────────────────────────
   for (const [i, row] of rows.entries()) {
-    const [first, second] = row.toLowerCase().trim().split('_');
+    const [first, second] = row.toLowerCase().trim().split("_");
     const output = `${first}${second.replace(
       second[0],
       second[0].toUpperCase()
     )}`;
-    console.log(`${output.padEnd(20)}${'✅'.repeat(i + 1)}`);
+    // HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
+    console.log(`${output.padEnd(20)}${"✅".repeat(i + 1)}`);
   }
 });
 */
-/*
-
-
-
-
-
-
 //ANCHOR Working With Strings part3
 /* -------------------------------------------------------------------------- */
 /*                         Working With Strings part3                         */
@@ -702,5 +686,4 @@ for (const [key, score] of entries) {
 // for (const [i, el] of menu.entries()) {
 //   console.log(`${i + 1} : ${el}`);
 // }
-
-// // console.log([...menu.entries()]);
+// // console.log([...menu.entries()])
